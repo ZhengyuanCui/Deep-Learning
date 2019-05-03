b9ec4a50b1f235c64b925d65decd250a91c26479
Zhenguan Cui <zhengyuancui1998@gmail.com>
2019-05-02 23:59:20 -0400

    VGG

--- CNN/VGG/VGG19.py
+++ CNN/VGG/VGG19.py
@@ -9,9 +9,13 @@ import time
 import math
 from collections import OrderedDict
 
-class VGG16(nn.module):
+BATACH_SIZE = 100
+LEARNING_RATE = 0.01
+EPOCH = 10
+
+class VGG19(nn.module):
     def __init__(self):
-        super(VGG16,self).__inte__()
+        super(VGG19,self).__inte__()
         #conv layer
         self.conv = nn.Sequential(
 
@@ -110,4 +114,49 @@ class VGG16(nn.module):
         out = self.fc(out)
         return out
 
-        
+def train(trainloader, net, criterion, optimizer, device):
+    for epoch in range(EPOCH):
+        start = time.time()
+        running_loss = 0
+        for i, (images, labels) in enumerate(trainloader):
+            images = images.to(device)
+            lables = lables.to(device)
+
+            optimizer.zero_grad()
+            output = net()
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 100 = 99:
+                end = time.time()
+                print('[epoch %d, iter %5d] loss:%.3f time: %.3f' %
+                        epoch+1, i+1, running_loss/100, end-start)
+                start = time.time()
+                running_loss = 0
+    print('Finish training')
+
+
+def test(testloader, net):
+    correct = 0
+    total = 0
+    with torch.no_grad:
+        for images, labels in testloader:
+             images = images.to(device)
+             lables = lables.to(device)
+
+             output = net(images)
+             _,predict = torch.max(output.data,1)
+             total += lables.size(0)
+             correct += (predict ==labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
+
+def main():
+    device = ('cuda' if torch.cuda.is_available() else 'cpu')
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean = [0.5,0.5,0.5],
+                             std = [0.25,0.25,0.25]),
+        ])
